ab7481b498e01c7d20287c23473acc00b0b3f58f
samsm-turner <samuel.schenkman-moore@turner.com>
2015-02-03 17:28:09 -0500

    Updated for Rails 4.1

--- lib/generators/padlock_generator.rb
+++ lib/generators/padlock_generator.rb
@@ -1,12 +1,18 @@
-require "rails/generators/active_record"
+require 'rails/generators'
+require 'rails/generators/migration'
 
 module Padlock
   module Generators
     class PadlockGenerator < ::Rails::Generators::Base
+      include Rails::Generators::Migration
       source_root File.expand_path("../templates", __FILE__)
 
+      def self.next_migration_number(path)
+        Time.now.utc.strftime("%Y%m%d%H%M%S")
+      end
+
       def copy_migration
-        copy_file 'migration.rb', "db/migrate/create_padlocks.rb"
+        migration_template 'migration.rb', "db/migrate/create_padlocks.rb"
       end
     end
   end
